e4e67b30832ccad1c7ae7e65bfb66d0cd1f95ac4
zjhch123 <416836508@qq.com>
2017-07-01 00:26:03 +0800

    清除 礼物

--- popup.js
+++ popup.js
@@ -11,6 +11,17 @@
     });
   };
 
+  // var _fiter = curry(function(f, dom) {
+  //   return dom.filter(f);
+  // });
+
+  // var _hideAdv = curry(function(dom) {
+  //   dom.style.display = "none";
+  //   return dom;
+  // });
+
+  // var hide = compose(_map(_hideAdv), filter(function() { return true; }));
+
   hideAdv('.aside_region.my_app.j_encourage_entry');
   hideAdv('#thread_list>li', function(ele) {
     if(!ele.getAttribute("data-field")) {
@@ -23,4 +34,5 @@
   hideAdv('.aside_region.celebrity');
   hideAdv('.aside_region.app_download_box');
   hideAdv('.topic_list_box');
+  hideAdv('.gift-goin');
 })()
