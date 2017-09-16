1a0cd3bbcc294514859bff8d7f2b64908e571e04
zjhch123 <416836508@qq.com>
2017-09-16 10:43:29 +0800

    适配最新百度贴吧页面规则

--- popup.js
+++ popup.js
@@ -11,17 +11,6 @@
     });
   };
 
-  // var _fiter = curry(function(f, dom) {
-  //   return dom.filter(f);
-  // });
-
-  // var _hideAdv = curry(function(dom) {
-  //   dom.style.display = "none";
-  //   return dom;
-  // });
-
-  // var hide = compose(_map(_hideAdv), filter(function() { return true; }));
-
   hideAdv('.aside_region.my_app.j_encourage_entry');
   hideAdv('#thread_list>li', function(ele) {
     if(!ele.getAttribute("data-field")) {
@@ -30,14 +19,9 @@
       return false;
     }
   });
-  hideAdv('.aside .celebrity');
-  hideAdv('.aside .app_download_box');
-  hideAdv('.topic_list_box');
-  hideAdv('.forum_recommend');
-  hideAdv('.gift-goin');
-  hideAdv(".thread_recommend");
-  hideAdv('.tbui_slideshow_container');
-  hideAdv('.right_section .celebrity');
-  hideAdv('.right_section .app_download_box');
-  hideAdv('.right_section .video_aside');
+  hideAdv('.head_main .head_content .gift-goin');
+  hideAdv('.aside_region.celebrity');
+  hideAdv('.aside_region.app_download_box');
+  hideAdv('#pagelet_frs-aside\\/pagelet\\/aside .topic_list_box');
+  hideAdv('.content_leftList #pagelet_live\\/pagelet\\/live');
 })()
